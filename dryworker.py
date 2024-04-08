a496d21ec776285b00f5486fc22eb342c86439e9
Chang <me@yukimuon.com>
2024-04-08 18:55:34 -0400

    Update post processing code

--- dryworker.py
+++ dryworker.py
@@ -12,37 +12,52 @@ import glob
 import requests
 import ntpath
 
-from assemblage.consts import BINPATH, PDBPATH, TASK_TIMEOUT_THRESHOLD, BuildStatus, MAX_MQ_SIZE
-# from assemblage.worker.base_worker import BasicWorker
 from assemblage.worker import build_method
 from assemblage.worker.find_bin import find_elf_bin
 from assemblage.worker.profile import AWSProfile
-# from assemblage.protobufs.assemblage_pb2 import getBuildOptRequest
 from assemblage.worker.build_method import DefaultBuildStrategy
 
 from vcpkg_functions import *
 
-
+VCPKG_COMMIT = "5fa0f075ea51f305b627ecd5e050a363707353ff"
+VCPKG_PATH = rf"C:\Users\yukim\OneDrive\Documents\vcpkg"
 
 mode = "release"
 compiler_version = 'v142'
 library = "x86/x64"
 
-def taskwrap(url, opt):
+def taskwrap(url, opt, version):
     msg, code, clonedir = clone_data(url, opt, mode)
     try:
         run_build(url, clonedir, mode, library, opt,
-					"", "", compiler_version)
+					"", "", compiler_version, version)
     except:
         pass
 
 
 import json
 projects = []
-with open("projects.json", "r") as f:
+with open("db.json", "r") as f:
     projects = json.load(f)
-import random
-random.shuffle(projects)
+
 for project in projects:
-    for optimization_level in ["Od", "O1", "O2", "Ox"]:
-        taskwrap(project, optimization_level)
+    for optimization_level in ["Od"]:
+        repo = project
+        version_byvcpkg = "?"
+        if os.path.isfile(rf"{VCPKG_PATH}\ports\{repo}\vcpkg.json"):
+            repodata = json.load(open(rf"{VCPKG_PATH}\ports\{repo}\vcpkg.json"))
+        else:
+            if os.path.isfile(rf"{VCPKG_PATH}\ports\{repo}\CONTROL"):
+                fcontent = open(rf"{VCPKG_PATH}\ports\{repo}\CONTROL").readlines()
+                for line in fcontent:
+                    if "Version:" in line:
+                        version_byvcpkg = line.strip()
+
+        if 'version' in repodata:
+            version_byvcpkg = (repodata['version'])
+        if "version-semver" in repodata:
+            version_byvcpkg = repodata['version-semver']
+        if "version-string" in repodata:
+            version_byvcpkg = repodata['version-string']
+
+        taskwrap(project, optimization_level, version_byvcpkg)
--- vcpkg_functions.py
+++ vcpkg_functions.py
@@ -11,6 +11,7 @@ import glob
 # import grpc
 import requests
 import ntpath
+from assemblage.worker.build_method_new import post_processing_pdb
 
 
 def clone_data(repo, optimization_level, mode):
@@ -23,15 +24,10 @@ def clone_data(repo, optimization_level, mode):
 	logging.info("Clone called")
 	return b'No need for clone', 1, dest_path
 
-def post_build_hook(dest_binfolder, build_mode, library, repoinfo, toolset,
-					optimization):
-	""" Postprocess the pdb """
-
-	return
 
 
 def run_build(repo, target_dir, build_mode, library, optimization,
-					slnfile, platform, compiler_version):
+					slnfile, platform, compiler_version, version=None):
 	""""""
 	logging.info(f" >>> Building {repo} ...")
 	triplet_cpu_arch = "x64"
@@ -50,64 +46,24 @@ def run_build(repo, target_dir, build_mode, library, optimization,
 	triplet_flags["VCPKG_CXX_FLAGS_RELEASE"] = f"/{optimization}"
 	triplet_flags["VCPKG_C_FLAGS_RELEASE"] = f"/{optimization}"
 
-
+	builddir = os.path.join(target_dir, "build"+os.urandom(8).hex())
+	bindir = os.path.join(target_dir, "bin"+os.urandom(8).hex())
 	with open(triplet_path, "w") as f:
 		for x in triplet_flags:
 			f.write(f"set({x} {triplet_flags[x]})\n")
-	cmd = f"vcpkg install {repo} --overlay-triplets={target_dir}/triplets --x-install-root={target_dir} --triplet {triplet_cpu_arch}-{optimization}-windows"
+	
+	cmd = f"vcpkg install {repo} --overlay-triplets={target_dir}/triplets --x-install-root={builddir} --triplet {triplet_cpu_arch}-{optimization}-windows --x-packages-root={bindir}"
 	logging.info(cmd)
-	os.system(cmd)
-
-	os.remove(triplet_path)
-	triplet_cpu_arch = "x86"
-	triplet_path = os.path.relpath(os.path.join(target_dir, "triplets", f"{triplet_cpu_arch}-{optimization.lower()}-windows.cmake"))
-	triplet_flags = {"VCPKG_TARGET_ARCHITECTURE": triplet_cpu_arch, "VCPKG_CRT_LINKAGE":"dynamic", "VCPKG_LIBRARY_LINKAGE":"dynamic"}
-	if build_mode.lower()=="release":
-		triplet_flags["VCPKG_BUILD_TYPE"] = "release"
-	else:
-		triplet_flags["VCPKG_BUILD_TYPE"] = "debug"
-	triplet_flags["CMAKE_CXX_FLAGS"] = f"/{optimization}"
-	triplet_flags["CMAKE_C_FLAGS"] = f"/{optimization}"
-	triplet_flags["VCPKG_CXX_FLAGS"] = f"/{optimization}"
-	triplet_flags["VCPKG_C_FLAGS"] = f"/{optimization}"
-	triplet_flags["CMAKE_CXX_FLAGS_RELEASE"] = f"/{optimization}"
-	triplet_flags["CMAKE_C_FLAGS_RELEASE"] = f"/{optimization}"
-	triplet_flags["VCPKG_CXX_FLAGS_RELEASE"] = f"/{optimization}"
-	triplet_flags["VCPKG_C_FLAGS_RELEASE"] = f"/{optimization}"
-
-
-	with open(triplet_path, "w") as f:
-		for x in triplet_flags:
-			f.write(f"set({x} {triplet_flags[x]})\n")
-	cmd = f"vcpkg install {repo} --overlay-triplets={target_dir}/triplets --allow-unsupported --triplet {triplet_cpu_arch}-{optimization}-windows"
-	logging.info(cmd)
-	os.system(cmd)
+	try:
+		os.system(cmd)
+	except:
+		pass
+	post_processing_pdb(
+		bindir, build_mode, library=library, repoinfo={"url":repo, "updated_at": version}, toolset="v142",
+				optimization=optimization, source_codedir=target_dir, commit=version, movedir=f"Binaries/{repo}-{triplet_cpu_arch}-{optimization}-{version.replace(':','')}({compiler_version})")
+	shutil.rmtree(r"Builds")
 
 	return
 
-def run_arm_build(repo, target_dir, build_mode, library, optimization,
-					slnfile, platform, compiler_version):
-	""""""
-	logging.info(f" >>> Building {repo} ...")
-	triplet_cpu_arch = "arm64"
-	if os.path.isdir(target_dir):
-		return
-	triplet_path = os.path.relpath(os.path.join(target_dir, "triplets", f"{triplet_cpu_arch}-{optimization.lower()}-windows.cmake"))
-	triplet_flags = {"VCPKG_TARGET_ARCHITECTURE": triplet_cpu_arch, "VCPKG_CRT_LINKAGE":"dynamic", "VCPKG_LIBRARY_LINKAGE":"dynamic"}
-	triplet_flags["VCPKG_BUILD_TYPE"] = "release"
-	triplet_flags["CMAKE_CXX_FLAGS"] = f"/{optimization}"
-	triplet_flags["CMAKE_C_FLAGS"] = f"/{optimization}"
-	triplet_flags["VCPKG_CXX_FLAGS"] = f"/{optimization}"
-	triplet_flags["VCPKG_C_FLAGS"] = f"/{optimization}"
-	triplet_flags["CMAKE_CXX_FLAGS_RELEASE"] = f"/{optimization}"
-	triplet_flags["CMAKE_C_FLAGS_RELEASE"] = f"/{optimization}"
-	triplet_flags["VCPKG_CXX_FLAGS_RELEASE"] = f"/{optimization}"
-	triplet_flags["VCPKG_C_FLAGS_RELEASE"] = f"/{optimization}"
 
-	with open(triplet_path, "w") as f:
-		for x in triplet_flags:
-			f.write(f"set({x} {triplet_flags[x]})\n")
-	cmd = f"vcpkg install {repo} --overlay-triplets={target_dir}/triplets --x-install-root={target_dir} --triplet {triplet_cpu_arch}-{optimization}-windows"
-	logging.info(cmd)
-	os.system(cmd)
 
